847893a3cdbbd3ee1157bd6b0551b1d53806cbf7
Fredrik Bondesson <fredrik.bondesson@gmail.com>
2021-12-12 12:14:17 +0100

    Added example text
    for my initial ide

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,7 +1,77 @@
 import timeit
-# from typing import DefaultDict
 from collections import defaultdict
 import util
+
+
+"""start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+
+{'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b'], 'end': ['A', 'b']}
+
+
+['start', 'A', 'c', 'b', 'end']
+['start', 'A', 'c', 'b', 'end']
+['start', 'b', 'A', 'd', 'end']
+['start', 'b', 'A', 'd', 'end']
+
+
+start A c
+ start A c A
+  start A c A c -> går inte redan kört c
+  start A c A b
+   start A c A b A
+    start A c A b A c -> går inte redan kört c
+    start A c A b A b -> går inte redan kört b
+    start A c A b A end
+   start A c A b d
+    start A c A b d b -> går inte redan kört b
+   start A c A b end
+  start A c A end  
+start A b
+ start A b A
+  start A b A c
+   start A b A c A
+    start A b A c A c -> går inte redan kört c
+    start A b A c A b -> går inte redan kört b
+    start A b A c A end
+  start A b A b -> går inte redan kört b
+  start A b A end 
+ start A b d
+  start A b d b -> går inte redan kört b
+ start A b end
+start A end
+
+start,b,A
+ start,b,A,c
+   start,b,A,c,A
+    start,b,A,c,A,c -> går inte redan kört c
+    start,b,A,c,A,b -> går inte redan kört b
+    start,b,A,c,A,end
+ start,b,A,b -> går inte redan kört b
+ start,b,A,end
+start,b,d
+ start,b,d,b -> går inte redan kört b
+start,b,end
+
+====>
+
+start,A,c,A,b,A,end
+start,A,c,A,b,end
+start,A,c,A,end  
+start,A,b,A,c,A,end
+start,A,b,A,end 
+start,A,b,end
+start,A,end
+start,b,A,c,A,end
+start,b,A,end
+start,b,end
+"""
+
 
 
 INPUT_SMALL="""start-A
